9b73f6d0271560e571ef35b56c5179d221658b50
Harddik Bafna <zardian018@gmail.com>
2023-06-08 23:49:39 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,279 @@
+const logotext = "Zardian";
+const meta = {
+	title: "Harddik Bafna",
+	description: "my portfolio",
+};
+
+const introdata = {
+	title: "I'm Harddik Bafna",
+	animated: {
+		first: "Game Developer",
+		second: "3D Modeler",
+		third: "Machine Learning Enthusiast",
+	},
+	description:
+		"I am an enthusiastic and versatile individual with a strong background in game development, 3D modeling, and machine learning. Fuelled by a deep passion for learning and coding, I constantly seek new challenges and love to explore innovative ideas. With my diverse skill set, I am dedicated to creating captivating and immersive experiences in the digital world.",
+	your_img_url: "#",
+};
+
+const dataabout = {
+	title: "A bit about my self",
+	aboutme:
+		"I am a dynamic professional with hands-on experience gained through internships, honing my skills in game development, 3D modeling, and machine learning. My relentless pursuit of knowledge in rapidly evolving technologies keeps me at the forefront of industry trends. With a creative mindset, I thrive on pushing the boundaries of innovation. From crafting captivating experiences to adapting to the ever-changing tech landscape, I am committed to delivering cutting-edge solutions.",
+};
+const worktimeline = [
+	{
+		jobtitle: "Unity Developer (Intern)",
+		where: "Alfaleus Technologies Pvt. Ltd",
+		date: "Sept' 22 - Feb' 23",
+	},
+	{
+		jobtitle: "Unity Game Developer & 3D Modeler (Intern)",
+		where: "Vifr Tech",
+		date: "July' 22 - Sept' 22",
+	},
+	{
+		jobtitle: "Android App Developer (Intern)",
+		where: "AJ Pro Cap",
+		date: "May' 22 - July' 22",
+	},
+	// {
+	// 	jobtitle: "Designer of week",
+	// 	where: "ALquds",
+	// 	date: "2019",
+	// }
+];
+const education = [
+	{
+		title: "Vellore Institute of Technology (VIT), Vellore (2020-2024) ",
+		description: "CGPA: 8.36",
+	},
+];
+
+const skills = [
+	{
+		name: "Machine Learning (ML)",
+	},
+	{
+		name: "Artificial Intelligence (AI)",
+	},
+
+	{
+		name: "Game Development",
+	},
+	{
+		name: "Unity / VR Development",
+	},
+	{
+		name: "Android App Development",
+	},
+	{
+		name: "Blender",
+	},
+	{
+		name: "Java",
+	},
+	{
+		name: "Python",
+	},
+	{
+		name: "C/C++",
+	},
+	{
+		name: "C#",
+	},
+	{
+		name: "SQL",
+	},
+	{
+		name: "React JS",
+	},
+	{
+		name: "Javascript",
+	},
+	{
+		name: "Solidity",
+	},
+	{
+		name: "Matlab",
+	},
+	{
+		name: "Network and Communication",
+	},
+	{
+		name: "Operating Systems",
+	},
+	{
+		name: "Data Structures",
+	},
+	{
+		name: "Natural Language Processing (NLP)",
+	},
+	{
+		name: "Sci-kit learn",
+	},
+	{
+		name: "INK",
+	},
+];
+
+const services = [
+	{
+		title: "Unity Developer (Alfaleus Technologies Pvt. Ltd)",
+		description:
+			"During my 5-month tenure as a Unity Developer, I had the opportunity to contribute to a groundbreaking project aimed at assisting individuals with low vision. Low vision, a condition not correctable by conventional means, doesn't entail complete blindness as patients retain sensitivity to light. Working with the Pico G24K E device, I played a vital role in developing features like fisheye and inline filters, grayscale and inverse grayscale modes, leveraging shaders and post-processing techniques. Additionally, I implemented zoom-in and zoom-out functionalities, brightness adjustment, and audio cues to facilitate patient engagement. This internship was both enjoyable and enlightening, providing me with invaluable knowledge and experience.",
+	},
+	{
+		title: "Unity Game Developer and 3D Modeler (Vifr Tech)",
+		description:
+			"During my 2-month stint at VIFR, I had the privilege of working on the HALARA project, an initiative dedicated to creating immersive VR games for children with autism, facilitating their comprehension and learning. Together, we developed a total of 10 modules, with 4 of them being my sole creations, encompassing everything from core mechanics to game implementation. I delved into various cutting-edge concepts, such as raycasting, XR-Rig, trigger and collision events, and even familiarized myself with PXR Manager and XR-input management system using Tobii initializer. Furthermore, I had the opportunity to explore the realm of 3D modeling, effectively applying those skills to enhance their projects. This experience was incredibly enjoyable and rewarding, allowing me to now integrate that knowledge into my own game development endeavors.",
+	},
+	{
+		title: "Android App Developer (AJ Pro Cap)",
+		description:
+			"As an intern at a counseling firm that supports startup ventures, I delved into the world of Android app development. This experience marked my first foray into a professional work environment, and I thoroughly enjoyed every aspect of it. During my tenure, I had the opportunity to create a business card app specifically tailored for the firm. This endeavor allowed me to gain practical skills in Android app development while contributing to the firm's digital presence.",
+	},
+];
+
+const services2 = [
+	{
+		title: "1st Position in hackathon ODYSSEY",
+		description:
+			"I participated in my first ever Hackathon, Odyssey and secured 1st place.I participated in a team of 4 with my colleagues. Our project idea, MAXBAY, was gamification of physiotherapy to provide treatments to the needy in such a way which they find more interactive, interesting and enjoyable. We detect the gestures made by the patient using appropriate sensors and further process that data on to Unity Game Engine and move the player accordingly for a fun and engaging product. The applications of this product are numerous and it can be used as a full body physiotherapy product to treat patients.",
+	},
+	{
+		title: "3rd Position in first ever ZTM Game Jam",
+		description:
+			"I enthusiastically took part in the ZTM Game Jam, an exhilarating competition centered around the theme of the AI revolution. Guided by my passion for game development, I conceptualized and created a captivating game where AI, having attained sentience, rebelled against humanity. In this thrilling experience, the player's mission was to eliminate AI bots and restore freedom to mankind. The competition proved to be a fantastic learning opportunity, and despite participating as a solo developer among teams, I proudly secured the 3rd position. This achievement stands as a testament to my dedication, skills, and ability to deliver a compelling gaming experience.",
+	},
+];
+
+const certificates = [
+	{
+		title: "Ultimate Guide to game development with unity",
+		description: "by: Johnathan Weinberg",
+		link: "https://drive.google.com/file/d/1daw01dCvkIUwmjmUWOAg76qK3L7iK0Hc/view",
+	},
+	{
+		title: "Complete C# unity game developer 2D",
+		description: "by: Rick Davidson",
+		link: "https://drive.google.com/file/d/1uwpKeKuES5HtxTPsJhImBTUUxUG6sx79/view",
+	},
+];
+const dataportfolio = [
+	{
+		img: "Images/Connan.jpg",
+		description:
+			"Connan's Odyssey is my debut Android platformer game available on the Play Store. It features 15 levels, 5 diverse playable characters with unique abilities, multiple enemy types, integrates ads and player data storage",
+		link: "https://play.google.com/store/apps/details?id=com.Zardian.ConansOdyssey",
+	},
+	{
+		img: "Images/sniffer2.png",
+		description:
+			"Together with my friends, we developed a packet sniffer that monitors and captures two-way network traffic, displaying the captured data with headers using TCP, UDP, and ICMP protocols. The project utilized Python3, Linux OS, and modules like Struct, sockets, argparse, coloroma, and textwrap.",
+		link: "https://github.com/Gaurav9912/Packet-Sniffer",
+	},
+	{
+		img: "Images/gridbox.png",
+		description:
+			"In my Valorant-inspired game, gridbox-shooter, players experience raycast-based shooting mechanics, weapon variety with unique animations, enemy AI, particle effects, and dynamic damage calculations. Creating this project was a challenging and educational endeavor.",
+		link: "https://sharemygame.com/@Zardian/gridbox-shooter",
+	},
+	{
+		img: "Images/blogapp.png",
+		description:
+			"A React.js website with hooks, facilitating blog creation, reading, and deletion. Users can easily add, browse, and remove blogs, ensuring a seamless and interactive experience for content creators and readers.",
+		link: "https://github.com/Zardian18/Z-Blog",
+	},
+	{
+		img: "Images/darts.png",
+		description:
+			"Darts is a unique game with split-screen functionality for enhanced precision, self-created 3D models in Blender, and accurate scoring mechanics using collider prefabs for seamless collision detection and scoring updates.",
+		link: "https://sharemygame.com/@Zardian/dartgame",
+	},
+
+	{
+		img: "Images/dai.png",
+		description:
+			"In my game, dAI, players challenge a world ruled by sentient AI, battling against their control to restore freedom. Designed for the ZTM game jam, the objective is to strategically outwit and overcome relentless bots for ultimate triumph.",
+		link: "https://zardian018.itch.io/dai",
+	},
+
+	{
+		img: "Images/mainMenuBomb.png",
+		description:
+			"This mobile game project offered a delightful experience and allowed me to learn touch and slide functionality. Players navigate by sliding their fingers, dodging bombs while exploring the captivating implementation of infinite parallax in the main menu.",
+		link: "https://sharemygame.com/@Zardian/bombdropper",
+	},
+	{
+		img: "Images/zombieCrasher.png",
+		description:
+			"Zombie Crasher is an infinite runner game featuring shooting mechanics and obstacle destruction. It incorporates infinite platform generation, collision detection, health-bar system, skin customization, and a functional pause menu. Additionally, it served as inspiration for my venture into 3D modeling.",
+		link: "https://sharemygame.com/@Zardian/zombie-crasher",
+	},
+	{
+		img: "Images/goblinHunter.png",
+		description:
+			"In this game, I animated 3D models with triggers, implementing multiple animations for each state. A combat system inspired by RPGs was incorporated, utilizing animation events and sphere-casting. Player movement was facilitated through a character controller.",
+		link: "#",
+	},
+
+	{
+		img: "Images/heartDisease.png",
+		description:
+			"Heart Disease Prediction: Employing scikit-learn, I explored multiple ML models, comparing their performance using graphs and illustrations. After hyperparameter tuning with grid search CV, my project achieved an impressive 89% accuracy. A rewarding journey unveiling the potential of machine learning in diagnosing heart disease.",
+		link: "https://github.com/Zardian18/heart-disease-predictor",
+	},
+	{
+		img: "Images/galaxy.png",
+		description:
+			"My first UNITY game: a 2.5D endless scrolling adventure. Implemented prefab instantiation, Quaternions, power-up UI icons, scoring based on distance, local multiplayer, difficulty settings, player prefs, and dynamic health states. A valuable learning experience and a stepping stone for future game development.",
+		link: "https://sharemygame.com/@Zardian/galaxy-shooter",
+	},
+	{
+		img: "Images/loneSur1.jpg",
+		description:
+			"Tap to move and shoot! This casual arcade game for Android, available on the Play Store, features 4 character choices, customizable controls, and integrated ads through IronSource and Unity Mediation. Engaging gameplay awaits!",
+		link: "#",
+	},
+	{
+		img: "Images/quiz1.png",
+		description:
+			"Anime Quiz: Engage in a captivating quiz game designed for anime enthusiasts. Randomly ordered questions stored as scriptable objects, limited time to answer, highlighted correct answers, and an intriguing circular timer.",
+		link: "https://github.com/Zardian18/QuizGame",
+	},
+];
+
+const contactConfig = {
+	YOUR_EMAIL: "zardian018@gmail.com",
+	YOUR_FONE: "+91 6353806456",
+	description:
+		"If you find my work and profile interesting, don't hesitate to get in touch with me. I'm constantly involved in new projects to enhance my skills and stay updated with the latest developments. I'm eager to explore new opportunities that allow me to further expand my expertise. Feel free to reach out to me through the following links",
+	// creat an emailjs.com account
+	// check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+	YOUR_SERVICE_ID: "service_9e4z7md",
+	YOUR_TEMPLATE_ID: "template_3vc944f",
+	YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+	github: "https://github.com/Zardian18",
+	facebook: "https://facebook.com",
+	linkedin: "https://www.linkedin.com/in/harddik-bafna/",
+	twitter: "https://twitter.com",
+};
+export {
+	meta,
+	dataabout,
+	dataportfolio,
+	worktimeline,
+	skills,
+	services,
+	introdata,
+	contactConfig,
+	socialprofils,
+	logotext,
+	services2,
+	education,
+	certificates,
+};
