c36bae91827f889f2878a6da138029a3d2b1a710
Harddik Bafna <zardian018@gmail.com>
2023-06-09 12:48:40 +0530

    error handling

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Route, Routes} from "react-router-dom";
-import withRouter from "../hooks/withRouter"
+import { Route, Routes } from "react-router-dom";
+import withRouter from "../hooks/withRouter";
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
 import { ContactUs } from "../pages/contact";
@@ -9,34 +9,34 @@ import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const AnimatedRoutes = withRouter(({ location }) => (
-  <TransitionGroup>
-    <CSSTransition
-      key={location.key}
-      timeout={{
-        enter: 400,
-        exit: 400,
-      }}
-      classNames="page"
-      unmountOnExit
-    >
-      <Routes location={location}>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </CSSTransition>
-  </TransitionGroup>
+	<TransitionGroup>
+		<CSSTransition
+			key={location.key}
+			timeout={{
+				enter: 400,
+				exit: 400,
+			}}
+			classNames="page"
+			unmountOnExit
+		>
+			<Routes location={location}>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/contact" element={<ContactUs />} />
+				<Route path="*" element={<Home />} />
+			</Routes>
+		</CSSTransition>
+	</TransitionGroup>
 ));
 
 function AppRoutes() {
-  return (
-    <div className="s_c">
-      <AnimatedRoutes />
-      <Socialicons />
-    </div>
-  );
+	return (
+		<div className="s_c">
+			<AnimatedRoutes />
+			<Socialicons />
+		</div>
+	);
 }
 
 export default AppRoutes;
