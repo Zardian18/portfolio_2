fc5fa827473772c55f576e858c4d07e3327a6b15
Harddik Bafna <zardian018@gmail.com>
2023-06-09 13:29:01 +0530

    bugs 4

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
 import { Home } from "../pages/home";
 import { Portfolio } from "../pages/portfolio";
@@ -8,60 +8,32 @@ import { About } from "../pages/about";
 import { Socialicons } from "../components/socialicons";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
-// const AnimatedRoutes = withRouter(({ location }) => (
-//   <TransitionGroup>
-//     <CSSTransition
-//       key={location.key}
-//       timeout={{
-//         enter: 400,
-//         exit: 400,
-//       }}
-//       classNames="page"
-//       unmountOnExit
-//     >
-//       <Routes location={location}>
-//         <Route exact path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/portfolio" element={<Portfolio />} />
-//         <Route path="/contact" element={<ContactUs />} />
-//         <Route path="*" element={<Home />} />
-//       </Routes>
-//     </CSSTransition>
-//   </TransitionGroup>
-// ));
-
-const NotFound = () => {
-	const navigate = useNavigate();
-
-	React.useEffect(() => {
-		navigate("/");
-	}, [navigate]);
-
-	return null;
-};
+const AnimatedRoutes = withRouter(({ location }) => (
+	<TransitionGroup>
+		<CSSTransition
+			key={location.key}
+			timeout={{
+				enter: 400,
+				exit: 400,
+			}}
+			classNames="page"
+			unmountOnExit
+		>
+			<Routes location={location}>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/contact" element={<ContactUs />} />
+				<Route path="*" element={<Home />} />
+			</Routes>
+		</CSSTransition>
+	</TransitionGroup>
+));
 
 function AppRoutes() {
 	return (
 		<div className="s_c">
-			<TransitionGroup>
-				<CSSTransition
-					key={location.key}
-					timeout={{
-						enter: 400,
-						exit: 400,
-					}}
-					classNames="page"
-					unmountOnExit
-				>
-					<Routes>
-						<Route exact path="/" element={<Home />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/portfolio" element={<Portfolio />} />
-						<Route path="/contact" element={<ContactUs />} />
-						<Route path="*" element={<NotFound />} />
-					</Routes>
-				</CSSTransition>
-			</TransitionGroup>
+			<AnimatedRoutes />
 			<Socialicons />
 		</div>
 	);
